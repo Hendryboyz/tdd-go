98ba15fe3973b2c741c25014629bac5005122602
Hendryboyz <Hendry.js1247@gmail.com>
2022-11-07 11:56:32 +0800

    feat: greeting with different lang

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+const SPANISH_LANG = "Spanish"
+const ENGLISH_GREETING = "Hello"
+const SPANISH_GREETING = "Hola"
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	const greetingPrefix = "Hello"
+	greetingPrefix := ENGLISH_GREETING
+	if language == SPANISH_LANG {
+		greetingPrefix = SPANISH_GREETING
+	}
 	return fmt.Sprintf("%s, %s", greetingPrefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,14 +4,19 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("When name is Henry Then greeting to Henry", func(t *testing.T) {
-		result := Hello("Henry")
+		result := Hello("Henry", "")
 		expected := "Hello, Henry"
-		assertCorrectString(t, result, expected)
+		assertCorrectString(t, expected, result)
 	})
 	t.Run("When name is empty Then name should be world", func(t *testing.T) {
-		result := Hello("")
+		result := Hello("", "")
 		expected := "Hello, World"
 		assertCorrectString(t, expected, result)
+	})
+	t.Run("When in Spanish", func(t *testing.T) {
+		actual := Hello("Henry", "Spanish")
+		expected := "Hola, Henry"
+		assertCorrectString(t, expected, actual)
 	})
 }
 
