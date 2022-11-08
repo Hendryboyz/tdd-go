aac7e114a60a72188f2a6461a01376d59a1d28ed
Hendryboyz <Hendry.js1247@gmail.com>
2022-11-08 10:57:25 +0800

    feat: adder test and basic logic

---
+++ integers/adder_test.go
@@ -0,0 +1,11 @@
+package integers
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	actual := Add(2, 2)
+	expected := 4
+	if actual != expected {
+		t.Errorf("expected '%d', but actual '%d'", expected, actual)
+	}
+}
