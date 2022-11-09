39191a134f1a87e877deace6d9ec6ff6f8d96f29
Hendryboyz <Hendry.js1247@gmail.com>
2022-11-09 10:58:31 +0800

    feat: repeat with strings package

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,19 @@
 package iteration
 
+import "strings"
+
 func Repeat(origin string, time int) string {
+	return repeatWithLibrary(origin, time)
+}
+
+func doRepeat(origin string, time int) string {
 	var repeated string
 	for len(repeated) != (len(origin) * time) {
 		repeated += origin
 	}
 	return repeated
+}
+
+func repeatWithLibrary(origin string, time int) string {
+	return strings.Repeat(origin, time)
 }
