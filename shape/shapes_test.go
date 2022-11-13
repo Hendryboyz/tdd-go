9334560994448e453bb4f5ed9cb34a896c6f90ef
Hendryboyz <hendry.js1247@gmail.com>
2022-11-13 21:50:24 +0800

    feat: calculate preimeter and area with Rectangle struct

--- shape/shapes_test.go
+++ shape/shapes_test.go
@@ -4,7 +4,11 @@ import "testing"
 
 func TestPreimeter(t *testing.T) {
 	t.Run("Given width and height", func(t *testing.T) {
-		actual := Preimeter(10.0, 10.0)
+		rect := Rectangle{
+			Width:  10.0,
+			Height: 10.0,
+		}
+		actual := Preimeter(rect)
 		expected := 40.0
 		assertFloat64(t, expected, actual)
 	})
@@ -18,7 +22,11 @@ func assertFloat64(t testing.TB, expected, actual float64) {
 
 func TestArea(t *testing.T) {
 	t.Run("Given width and height", func(t *testing.T) {
-		actual := Area(12.0, 6.0)
+		rect := Rectangle{
+			Width:  12.0,
+			Height: 6.0,
+		}
+		actual := Area(rect)
 		expected := 72.0
 		assertFloat64(t, expected, actual)
 	})
