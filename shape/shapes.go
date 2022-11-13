044ccbd15a990b9e8b7155845d99411c44d397e2
Hendryboyz <hendry.js1247@gmail.com>
2022-11-13 22:05:15 +0800

    refactor: use method to calculate rect and circle area

--- shape/shapes.go
+++ shape/shapes.go
@@ -1,5 +1,7 @@
 package shape
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +11,15 @@ func Preimeter(rect Rectangle) float64 {
 	return 2 * (rect.Width + rect.Height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+// func (receiverName ReceiverType) MethodName(args) returnType {}
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- shape/shapes_test.go
+++ shape/shapes_test.go
@@ -3,7 +3,7 @@ package shape
 import "testing"
 
 func TestPreimeter(t *testing.T) {
-	t.Run("Given width and height", func(t *testing.T) {
+	t.Run("Given Rectangle", func(t *testing.T) {
 		rect := Rectangle{
 			Width:  10.0,
 			Height: 10.0,
@@ -15,19 +15,30 @@ func TestPreimeter(t *testing.T) {
 }
 
 func assertFloat64(t testing.TB, expected, actual float64) {
+	t.Helper()
 	if actual != expected {
 		t.Errorf("Expect %.2f, but get %.2f", expected, actual)
 	}
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Given width and height", func(t *testing.T) {
+	t.Run("Given Rectangle", func(t *testing.T) {
 		rect := Rectangle{
 			Width:  12.0,
 			Height: 6.0,
 		}
-		actual := Area(rect)
+		actual := rect.Area()
 		expected := 72.0
+
+		if actual != expected {
+			t.Errorf("Expect %g, but get %g", expected, actual)
+		}
+	})
+
+	t.Run("Given Circle", func(t *testing.T) {
+		circle := Circle{Radius: 10.0}
+		actual := circle.Area()
+		expected := 314.1592653589793
 		assertFloat64(t, expected, actual)
 	})
 }
