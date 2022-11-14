3bb22c70dbb2713475a08e26b89390a5bf670971
Hendryboyz <hendry.js1247@gmail.com>
2022-11-14 21:26:13 +0800

    feat: test and calculate triangle area

--- shape/shapes_test.go
+++ shape/shapes_test.go
@@ -26,21 +26,23 @@ func TestArea(t *testing.T) {
 		t.Helper()
 		actual := shape.Area()
 		if actual != expected {
-			t.Errorf("Expect %g, but get %g", expected, actual)
+			t.Errorf("Area of %#v expect %g, but get %g", shape, expected, actual)
 		}
 	}
-	t.Run("Given Rectangle", func(t *testing.T) {
-		rect := Rectangle{
-			Width:  12.0,
-			Height: 6.0,
-		}
-		expected := 72.0
-		checkArea(t, rect, expected)
-	})
 
-	t.Run("Given Circle", func(t *testing.T) {
-		circle := Circle{Radius: 10.0}
-		expected := 314.1592653589793
-		checkArea(t, circle, expected)
-	})
+	areaTests := []struct {
+		name         string
+		shape        Shape
+		expectedArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, expectedArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, expectedArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, expectedArea: 36.0},
+	}
+
+	for _, testCase := range areaTests {
+		t.Run(testCase.name, func(t *testing.T) {
+			checkArea(t, testCase.shape, testCase.expectedArea)
+		})
+	}
 }
