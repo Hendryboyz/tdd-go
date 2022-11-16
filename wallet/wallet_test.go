377106a869713a4c72dcdc891bef2b553f570c8e
Hendryboyz <Hendry.js1247@gmail.com>
2022-11-16 10:36:47 +0800

    refactor: wallet test cases

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,36 +5,34 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("Deposit", func(t *testing.T) {
-		// arrange
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(64.2))
-
-		// action
+	assertBitcoin := func(t testing.TB, wallet Wallet, expected Bitcoin) {
+		t.Helper()
 		actual := wallet.Balance()
-		// fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-		// assert
-		expected := Bitcoin(64.2)
 		if actual != expected {
 			t.Errorf("Expected %s, but actual %s", expected, actual)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		// arrange
+		wallet := Wallet{}
+
+		// action
+		wallet.Deposit(Bitcoin(64.2))
+
+		// assert
+		assertBitcoin(t, wallet, Bitcoin(64.2))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		// Arrange
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(30.2))
+		wallet := Wallet{balance: Bitcoin(30.2)}
 
 		// Action
 		wallet.Withdraw(Bitcoin(15.2))
 
 		// Assert
-		actual := wallet.Balance()
-		expected := Bitcoin(15.0)
-		if actual != expected {
-			t.Errorf("Expected %s, but actual %s", expected, actual)
-		}
+		assertBitcoin(t, wallet, Bitcoin(15.0))
 	})
 
 }
