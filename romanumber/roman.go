6313539e7eb2b99b9e112e19cd5c3d11cbed95e3
Hendryboyz <Hendry.js1247@gmail.com>
2024-11-04 10:48:50 +0800

    feat: roman to arabic then reverse kata

---
+++ romanumber/roman.go
@@ -0,0 +1,51 @@
+package romannumber
+
+import (
+	"strings"
+)
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+
+	// A Builder is used to efficiently build a string using Write methods. It minimizes memory copying.
+	var romanNumber strings.Builder
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			romanNumber.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+	return romanNumber.String()
+}
+
+func ConvertToArabic(romanNumeral string) int {
+	arabic := 0
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(romanNumeral, numeral.Symbol) {
+			arabic += numeral.Value
+			// romanNumeral = romanNumeral[len(numeral.Symbol):]
+			romanNumeral = strings.TrimPrefix(romanNumeral, numeral.Symbol)
+		}
+	}
+	return arabic
+}
